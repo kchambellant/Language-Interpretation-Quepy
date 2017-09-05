65d35280bdb262aeb9042e8a3f8d31af7f9c7203
Killian CHAMBELLANT <killian.chambellant@insa-rouen.fr>
2017-09-05 13:12:51 +0200

    modification du main du proto2

--- Assistant/prototype2/wikidata/main.py
+++ Assistant/prototype2/wikidata/main.py
@@ -13,10 +13,27 @@ import datetime
 
 from SPARQLWrapper import SPARQLWrapper, JSON, XML
 
+
 def print_define(results, target, metadata=None):
     for result in results["results"]["bindings"]:
+        result[target]["value"]=result[target]["value"].replace("http://www.wikidata.org/entity/","")
         print result[target]["value"]
+        return result[target]["value"]
 
+
+def query_change(query, target, label=False):
+    query = query.replace(target, "?x0", 1).replace("?x0 ToGenerate ?x1.", "", 1)
+    target = "?x0"
+    if label:
+        query = query[:-2]+'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],fr". }'+"\n}\n"
+    return query, target
+
+
+metadico = {"whois":"print_personal"}
+
+
+def print_personal(URI):
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Quepy prototype number 1')
@@ -26,11 +43,11 @@ if __name__ == "__main__":
 
 
     prototype1 = quepy.install("wikidata")
+    
     target, query, metadata = prototype1.get_query(args.question)
+    query, target = query_change(query, target)
 
-    query = query.replace(target, "?x0", 1).replace("?x0 ToGenerate ?x1.", "", 1)
-    target = "?x0"
-    query = query[:-2]+'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],fr". }'+"\n}\n"
+    print("metadata: %s"%metadata)
     print query
     sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
     if target.startswith("?"):
@@ -42,4 +59,5 @@ if __name__ == "__main__":
             results = sparql.query().convert()
 
     print("print_define :")
-    print_define(results, target, metadata)
+    result = print_define(results, target, metadata)
+
