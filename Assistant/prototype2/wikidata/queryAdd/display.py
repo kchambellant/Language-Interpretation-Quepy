849fb1b89b9703ec7fffa25f0dca85bb3cc0f2cd
Killian CHAMBELLANT <killian.chambellant@insa-rouen.fr>
2017-09-06 09:24:58 +0200

    Ajout du module display.py

---
+++ Assistant/prototype2/wikidata/queryAdd/display.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def who_is(data):
+    str = ""
+    if data.has_key("metier"):
+        if type(data["metier"]) == list:
+            data["metier"] = ", ".join(data["metier"])
+            str_metier = "exerce les professions:"
+        else:
+            str_metier = "exerce la profession:"
+    else:
+        str_metier = ""
+        data["metier"] = ""
+    if data.has_key("genre"):
+        if data["genre"] == "masculin":
+            str = "%s %s %s. Il est né le %s dans le pays: %s" %(data["nom"], str_metier, data["metier"], data["age"], data["nation"])
+        elif "feminin":
+            str = "%s %s %s. Elle est née le %s dans le pays: %s" %(data["nom"], str_metier, data["metier"], data["age"], data["nation"])
+    return str
